dff817a55aee225234ccd8c9b10dcaa065e97314
CodingCody-1 <djcody07@gmail.com>
2022-04-24 21:54:41 -0400

    Fix Indents

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,12 @@ void loop() {
   delay(500);
 
   if (h >= 15 ){  //humidity greater than 15
-   digitalWrite(5, HIGH);
-   fan = 1;
+    digitalWrite(5, HIGH);
+    fan = 1;
   }
   else if (h <= 10){  //humidity less than 10
-   digitalWrite(5, LOW);
-   fan = 0;
+    digitalWrite(5, LOW);
+    fan = 0;
   }
   lcd.clear();
   lcd.setCursor(0,0);
